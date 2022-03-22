cf5814f7341ac068fc541ed8312b316a12cd5e82
ArturoDeLosSantos <arturodlscuellar@gmail.com>
2022-03-22 23:25:18 +0100

    cuarto commit y un Push

--- src/main/Test.java
+++ src/main/Test.java
@@ -4,8 +4,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola soy Arturo");
-		System.out.println("Estoy haciendo pruebas");
+		System.out.println("Voy a subir codigo");
+		
 
 	}
 
